47026fcecf051795c9a101a20a100eccbbc622ca
BLADE-HEDA <you@example.com>
2023-09-04 14:39:59 +0100

    problems encounterd and being resolved when importing images from json

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import productData from "../components/Data.json"
 
 const Products = () => {
+    // get the first 3 elemnts from the lists of products 
+    const homeProducts = productData.slice(0,3)
+    console.log('here are the home products',homeProducts);
+    
   return (
-    <div>
-      
+    <div className='px-8 ' >
+      <h1 className="head text-[black] text-[1.9em] font-[500] mb-3 ">OUR PRODUCTS</h1>
+      <p className=" text-[1.15em] text-start mr-2 mb-8">Check out our online shop for cute bee-themed apparel, unique bee suits, 
+      educational tools, gift certificates and more.</p>
+      {/* render the products */}
+      {
+        homeProducts.map((product,index)=>(
+            <div key={product.id} >
+                {/* display the new sign on a particular product in this case product 2 */}
+                {index===1 && <div className="">NEW</div>  }
+                <img src= alt={product.name} />
+                <h1 className="">{product.name} </h1>
+                <p className=""> ${product.price} </p>
+                <p className="">{product.description} </p>
+
+
+            </div>
+
+        ))
+      }
     </div>
   )
 }
