59327c904d73c95e5a1c3dec16e634a44f7c3c64
BLADE-HEDA <you@example.com>
2023-09-18 22:26:40 +0100

    news letter desktopt scrreen size bult successfuly

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -4,20 +4,24 @@ import Button from './Button';
 
 const Newsletter = () => {
   return (
-    <div className='px-[1.7em] mt-[3em] md:px-[3.5em] lg:px-[5em] ' >
+    <main className='px-[1.7em] mt-[3em] md:px-[3.5em] lg:px-[5em] lg:flex  ' >
+      <section className="flex-[50%] ">
       <img src={image} alt="image" />
+      </section>
+
+      <section className="flex-[50%] mt-0 ml-0  lg:mt-[4em] lg:ml-[3em] ">
       <h1 className="text-[black] text-start text-[2.1em] font-[900] mb-5 mt-[1.5em] mr-[2em]">NEWSLETTER SIGN UP</h1>
       <p className="text-[1.15em] text-start mr-2 mb-5">Neque porro quisquam est, qui dolorem ipsum quia dolor
        sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut
        labore et dolore magnam aliquam quaerat voluptatem dolores.</p>
-       <div className="mt-[3em]">
-        <div className="">
+       <article className="mt-[3em] md:flex ">
+        <div className="flex-[75%]">
             <input 
             type="text" 
             placeholder='Email Address'
             className="border-2 border-[#705B68] p-3.5 w-full bg-[#F7F3F0] text-[1.2em] mb-2 focus:outline-none" />
              </div>
-        <div className="w-full"> 
+        <div className="w-full flex-[25%] md:ml-2"> 
         <Button
         name='SUBSCRIBE'
         style={{
@@ -26,8 +30,9 @@ const Newsletter = () => {
         }}
         />
          </div>
-       </div>
-    </div>
+       </article>
+       </section>
+    </main>
   )
 }
 
