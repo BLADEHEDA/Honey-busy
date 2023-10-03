500b31cde8139f3f7473a1916332f496c8679996
BLADE-HEDA <you@example.com>
2023-10-03 12:15:57 +0100

    navbar stylings done , left to modify it by includeingthe styels

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars,faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { faBars,faCaretLeft,faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import logo from "../assets/New-logo-png1.png"
 import { Link } from 'react-router-dom'
 import "../App.css"
@@ -47,7 +47,31 @@ const postTypeLinks=[
   {name:"VIDEO", href:"" },
   {name:"NO SIDEBAR", href:"" },
 ]
-
+const listTypeLinks=[
+  {name:"STANDARD", href:"" },
+  {name:"GALLERY", href:"" },
+  {name:"GALLERY JOINED", href:"" },
+  {name:"MASONRY", href:"" },
+]
+const layoutLinks=[
+  {name:"TWO COLUMNS", href:"" },
+  {name:"THREE COLUMNS", href:"" },
+  {name:"THREE COLUMNS WIDE", href:"" },
+  {name:"FOUR COLUMNS", href:"" },
+  {name:"FOUR COLUMNS WIDE", href:"" },
+  {name:"FIVE COLUMNS", href:"" },
+  {name:"FIVE COLUMNS WIDE", href:"" },
+  {name:"SIX COLUMNS", href:"" },
+]
+const singleTypesLinks=[
+  {name:"SMALL IMAGES", href:"" },
+  {name:"BIG IMAGES", href:"" },
+  {name:"SMALL GALLERY", href:"" },
+  {name:"BIG GALLERY", href:"" },
+  {name:"SMALL MANSONRY", href:"" },
+  {name:"BIG MANSONRY", href:"" },
+  {name:"SLIDER", href:"" },
+]
 
 const Navbar = () => {
   const [navlink, setnaLinks]= useState(true)
@@ -194,8 +218,8 @@ const Navbar = () => {
         ] )
         }
           </div> 
-          {/* end  */}
           </div> 
+             {/* end  of product hover div and sunsections */}
        </div>
        <div className="navlin relative ">
         <p className="font-[500] text-[0.95em] text-[black]">BLOG</p>
@@ -246,13 +270,105 @@ const Navbar = () => {
           </div> 
           {/* end  */}
           
-        {/* end of changes postTypeLinks */}
+        {/* end of changes */}
        </div>
-       <div className="navlin">
+       {/* portfolio section and its hiiden subsections */}
+       <div className="navlin relative  ">
         <p className="font-[500] text-[0.95em] text-[black]">PORTFOLIO</p>
-      {/* hover-div */}
-        <div className=""></div> 
+        <div className="bg-[white] mt-[2.5em] border- border-[black] py-[1.2em] pl-[1.7em] 
+       pr-[15em absolute w-[14em] z-[999] shadow-md left-[-2em] hidden ">
+          {/* main hover */}
+          <div className="mb-2 borde border-[red] flex justify-between flex-row-reverse pr-[3em]">
+            <div className="">
+            <Link
+            to="/shoplist"
+            className='font-[500] text-[0.85em] text-[black] w-full ' > 
+            LIST TYPES
+            </Link>
+            </div>
+            <div className="">
+              <FontAwesomeIcon icon={faCaretLeft} className=' hover:ml-[0.2em] ' />
+            </div>
+          </div>
+          {/* subsection hover to be hidden */}
+          <div className="bg-[white] mt-[2.5em] border- border-[black] py-[1.2em] pr-[1.7em] 
+        absolute w-[14em] z-[999] shadow-md right-[14.005em] bottom-[-2.0em] top-[1em hidden text-end ">  
+        {listTypeLinks.map((listTypeLink,index)=>[
+          <div className="mb-2">
+            <Link
+            key={index}
+            to={listTypeLink.href}
+            className='font-[500] text-[0.85em] text-[black] w-full  '
+            >
+            {listTypeLink.name}
+            </Link>
+          </div>
+        ] )
+        }
+          </div> 
+          {/* end  */}
+          {/*nextmain hover  */}
+          <div className="mb-2 borde border-[red] flex justify-between flex-row-reverse pr-[3em]">
+            <div className="">
+            <Link
+            to="/shoplist"
+            className='font-[500] text-[0.85em] text-[black] w-full ' > 
+            LAYOUTS
+            </Link>
+            </div>
+            <div className="">
+              <FontAwesomeIcon icon={faCaretLeft} className=' hover:ml-[0.2em] ' />
+            </div>
+          </div>
+                 {/* subsection hover to be hidden //  */}
+        <div className="bg-[white] mt-[2.5em] border- border-[black] py-[1.2em] pr-[1.7em] 
+        absolute w-[14em] z-[999] shadow-md right-[14em] top-[-1em] hidden text-end  ">  
+        {layoutLinks.map((layoutLink,index)=>[
+          <div className="mb-2">
+            <Link
+            key={index}
+            to={layoutLink.href}
+            className='font-[500] text-[0.85em] text-[black] w-full '
+            >
+            {layoutLink.name}
+            </Link>
+          </div>
+        ] )
+        }
+          </div> 
+          {/* MAIN HOEVR 3 */}
+          <div className="mb-2 borde border-[red] flex justify-between flex-row-reverse pr-[3em]">
+            <div className="">
+            <Link
+            to="/shoplist"
+            className='font-[500] text-[0.85em] text-[black] w-full ' > 
+            SINGLE TYPES
+            </Link>
+            </div>
+            <div className="">
+              <FontAwesomeIcon icon={faCaretLeft} className=' hover:ml-[0.2em] ' />
+            </div>
+          </div>
+        {/* subsection hover to be hidden */}
+        <div className="bg-[white] mt-[2.5em] border- border-[black] py-[1.2em] pr-[1.7em] text-end 
+        absolute w-[14em] z-[999] shadow-md right-[14em] top-[1em] hidden ">  
+        {singleTypesLinks.map((shoPageLink,index)=>[
+          <div className="mb-2">
+            <Link
+            key={index}
+            to={shoPageLink.href}
+            className='font-[500] text-[0.85em] text-[black] w-full '
+            >
+            {shoPageLink.name}
+            </Link>
+          </div>
+        ] )
+        }
+          </div>
+          </div> 
+             {/* end  of product hover div and sunsections */}
        </div>
+       {/* portfolio end */}
       </article>
       <article className=" flex border- border-[red] flex-[20%] ml-[3em] justify-between ">
       <div className="">
